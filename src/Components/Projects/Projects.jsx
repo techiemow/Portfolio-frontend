50da64d2d7fc001d5833c653f325e8d1c1b4a118
techiemow <mowriyaabk@gmail.com>
2024-08-08 15:08:23 +0530

    some changes

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,51 +1,53 @@
 import React from 'react';
 import { Carousel, Card, Button } from 'react-bootstrap';
-import { MdNavigateNext } from 'react-icons/md';
 import { GrLinkNext, GrLinkPrevious } from 'react-icons/gr';
-import project1Image from '../../assets/documents/projects/eliteemart.jpeg';
-import project2Image from '../../assets/documents/projects/Bus-Booking.jpeg';
+import project1Image from '../../assets/documents/projects/emart3.png';
+import project2Image from '../../assets/documents/projects/busbooking.jpg';
 import project3Image from '../../assets/documents/projects/hotelbooking.png';
-import project4Image from '../../assets/documents/projects/petcare.png';
-
+import project4Image from '../../assets/documents/projects/pet3.jpeg';
+import project from "../../assets/documents/projects/project.png"
 const projects = [
   {
     title: 'Elite eMart',
     image: project1Image,
-    description: 'An e-commerce platform with a variety of products and seamless user experience.',
+    description: 'An e-store platform with a variety of electronic products, including the latest gadgets and accessories, offering a seamless user experience and a wide range of options to choose from.',
     website: 'https://eliteemart.com',
-    frontend: 'https://github.com/username/eliteemart-frontend',
-    backend: 'https://github.com/username/eliteemart-backend',
+    frontend: 'https://github.com/techiemow/Elite-Emart',
+    backend: 'https://github.com/techiemow/Elite-Emart-Backend',
   },
   {
     title: 'Bus Booking System',
     image: project2Image,
-    description: 'A complete bus booking system with real-time seat availability.',
+    description: 'A complete bus booking system with real-time seat availability, allowing users to book their tickets effortlessly, view schedules, and manage their bookings with ease.',
     website: 'https://busbooking.com',
-    frontend: 'https://github.com/username/busbooking-frontend',
-    backend: 'https://github.com/username/busbooking-backend',
+    frontend: 'https://github.com/techiemow/Bus-Booking-frontend',
+    backend: 'https://github.com/techiemow/Bus-Booking-Backend',
   },
   {
-    title: 'Hotel Booking System',
+    title: 'BookHaven',
     image: project3Image,
-    description: 'An advanced hotel booking system with features like room selection and payment integration.',
+    description: 'An advanced hotel booking system with features like integrated payment solutions, detailed room descriptions, and user reviews, making it easy to find and book the perfect stay.',
     website: 'https://hotelbooking.com',
-    frontend: 'https://github.com/username/hotelbooking-frontend',
-    backend: 'https://github.com/username/hotelbooking-backend',
+    frontend: 'https://github.com/techiemow/Hotel-Booking-FE',
+    backend: 'https://github.com/techiemow/Hotel-Booking-BE',
   },
   {
-    title: 'Pet Care App',
+    title: 'Paws and Claws App',
     image: project4Image,
-    description: 'An app to book pet care services with easy navigation and user-friendly interface.',
+    description: 'An app designed for booking pet care services, featuring easy navigation, a user-friendly interface, and various service options for pet grooming, boarding, and more.',
     website: 'https://petcare.com',
-    frontend: 'https://github.com/username/petcare-frontend',
-    backend: 'https://github.com/username/petcare-backend',
+    frontend: 'https://github.com/techiemow/Pet-sitting-app-Frontend',
+    backend: 'https://github.com/techiemow/Pet-Sitting-app',
   },
 ];
 
 const Projects = () => {
   return (
-    <div id="projects" className="container my-5">
-      <h1 className="text-center mb-4 " style={{color:"black"}}>Projects</h1>
+    <div id="projects" className="container my-5 p-5">
+    
+      <h1 className=' skills-heading text-center mb-4' style={{ color: "black" }}>
+            
+            <img src={project} alt='Skills' /> Projects </h1>
       <Carousel
         indicators={false}
         interval={3000}
@@ -55,29 +57,57 @@ const Projects = () => {
       >
         {projects.map((project, index) => (
           <Carousel.Item key={index}>
-            <div className="d-flex justify-content-around">
-              <Card style={{ width: '35%', height: '500px', transition: 'transform 0.5s ease' }} className="hover-shadow">
+            <div className="d-flex justify-content-around p-3">
+              <Card
+                style={{
+                  width: '35%',
+                  height: '500px',
+                  transition: 'transform 0.5s ease, box-shadow 0.5s ease',
+                  boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+                }}
+                className="hover-shadow"
+              >
                 <Card.Img
                   variant="top"
                   src={project.image}
                   alt={project.title}
-                  style={{ height: '350px', objectFit: 'cover' }}
+                  style={{ height: '290px', objectFit: 'cover' }}
                 />
                 <Card.Body>
                   <Card.Title>{project.title}</Card.Title>
-                  <Card.Text>{project.description}</Card.Text>
-                  <Button variant="primary" href={project.website} target="_blank" className="mr-2">
-                    Website
-                  </Button>
-                  <Button variant="secondary" href={project.frontend} target="_blank" className="mr-2">
-                    Front-end
-                  </Button>
-                  <Button variant="secondary" href={project.backend} target="_blank">
-                    Back-end
-                  </Button>
+                  <Card.Text style={{ height: '100px' }}>
+                    {project.description}
+                  </Card.Text>
+                  <div className="d-flex justify-content-between">
+                    <Button
+                      variant="primary"
+                      href={project.website}
+                      target="_blank"
+                      className="mr-2"
+                      style={{ marginRight: '8px' }}
+                    >
+                      
+                      Website
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      href={project.frontend}
+                      target="_blank"
+                      className="mr-2"
+                      style={{ marginRight: '8px' }}
+                    >
+                      Front-end
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      href={project.backend}
+                      target="_blank"
+                    >
+                      Back-end
+                    </Button>
+                  </div>
                 </Card.Body>
               </Card>
-              
             </div>
           </Carousel.Item>
         ))}
