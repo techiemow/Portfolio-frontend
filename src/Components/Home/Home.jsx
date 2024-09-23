bb950dcd60aaa8c2aae7de5aaf3dc827231ae71c
techiemow <mowriyaabk@gmail.com>
2024-09-23 19:17:37 +0530

    some changes with resume

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -122,7 +122,7 @@ const Home = () => {
                       variant="contained"
                       className="px-5 mx-1"
                       onClick={() => {
-                        window.open('https://drive.google.com/file/d/1pxCS_lpKIZ3QSDzFLYU2PSGqf3rABI6Q/view', '_blank');
+                        window.open('https://drive.google.com/file/d/1ba9VOzgyACKpJsMcfTmpj4m1IpZ0koqK/view', '_blank');
                       }}
                     >
                       CV <ArrowRightCircle size={25} />
